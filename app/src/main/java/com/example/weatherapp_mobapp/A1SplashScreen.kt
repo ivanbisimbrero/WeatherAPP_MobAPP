093b5ea838579b2b1c0fd43ef97006883ccf173f
ivanbisimbrero <alu.135046@usj.es>
2024-04-22 11:28:11 +0200

    Created activity_a1_splash_screen.xml and added the animations

--- app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
@@ -3,9 +3,11 @@ package com.example.weatherapp_mobapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class A1SplashScreen : AppCompatActivity() {
+
+    private val view by lazy {}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_a1_splash_screen)
     }
 }
---
+++ app/src/main/java/com/example/weatherapp_mobapp/A2MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp_mobapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class A2MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
