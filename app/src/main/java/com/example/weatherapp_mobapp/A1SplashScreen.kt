07097429d433e35e6e11acc4aad508e3578e76f8
ivanbisimbrero <alu.135046@usj.es>
2024-04-22 11:52:33 +0200

    Enabled ViewBinding for gradle.build and finished the animation after 3 seconds (by the moment).

--- app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
@@ -1,13 +1,23 @@
 package com.example.weatherapp_mobapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import com.example.weatherapp_mobapp.databinding.ActivityA1SplashScreenBinding
 
 class A1SplashScreen : AppCompatActivity() {
 
-    private val view by lazy {}
+    private val view by lazy { ActivityA1SplashScreenBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_a1_splash_screen)
+        super.onCreate(savedInstanceState);
+        setContentView(view.root);
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, A2MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
--- app/src/main/java/com/example/weatherapp_mobapp/A2MainActivity.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A2MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.weatherapp_mobapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherapp_mobapp.databinding.ActivityMainBinding
 
 class A2MainActivity : AppCompatActivity() {
+
+    private val view by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(view.root)
     }
 }
