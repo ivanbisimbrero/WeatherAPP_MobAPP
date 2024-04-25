70b91c0ca3e86cfe307dc780eaa51244165f1a7e
ivanbisimbrero <alu.135046@usj.es>
2024-04-25 12:54:38 +0200

    Permission for internet given, added necessary libraries, and started to code the API communication to retrieve data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("io.ktor:ktor-client-core:2.3.10")
+    implementation("io.ktor:ktor-client-cio:2.3.10")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A1SplashScreen.kt
@@ -3,21 +3,64 @@ package com.example.weatherapp_mobapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Looper
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.example.weatherapp_mobapp.databinding.ActivityA1SplashScreenBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.readText
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class A1SplashScreen : AppCompatActivity() {
 
     private val view by lazy { ActivityA1SplashScreenBinding.inflate(layoutInflater) }
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(view.root);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, A2MainActivity::class.java))
-            finish()
-        }, 3000)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            // If permissions not given, ask the user for it
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
+        // If given permissions, we retrieve the data from the API
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location ->
+                println("Latitud: " + location.latitude)
+                println("Longitud: " + location.longitude)
+
+                scope.launch {
+                    //val weatherData = fetchWeather(location.latitude, location.longitude)
+
+                    //TODO: manage the returned data
+                    //println(weatherData)
+
+                    withContext(Dispatchers.Main) {
+                        startActivity(Intent(this@A1SplashScreen, A2MainActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+    }
+
+    private suspend fun fetchWeather(lat: Double, lon: Double): String {
+        val client = HttpClient()
+        val response: HttpResponse = client.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$lat,$lon?unitGroup=metric&key=LRH7D4ZHU7LAWANGZBRQXPPGU")
+        val responseText: String = response.bodyAsText()
+        client.close()
+        return responseText
     }
 }
