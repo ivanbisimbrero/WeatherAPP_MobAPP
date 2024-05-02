14039ea594883af39bb537ede426d8071b019565
ivanbisimbrero <alu.135046@usj.es>
2024-05-02 20:17:28 +0200

    Refactored A3 and A4 with a generic class BaseCityListActivity

--- app/src/main/java/com/example/weatherapp_mobapp/A3FavouriteCitiesList.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A3FavouriteCitiesList.kt
@@ -1,65 +1,16 @@
 package com.example.weatherapp_mobapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.ListView
-import android.widget.SearchView
+import android.view.View
 import com.example.weatherapp_mobapp.databinding.ActivityA3FavouriteCitiesListBinding
 
-class A3FavouriteCitiesList : AppCompatActivity() {
+class A3FavouriteCitiesList : BaseCityListActivity() {
 
     private val view by lazy { ActivityA3FavouriteCitiesListBinding.inflate(layoutInflater) }
-    private var displayType = CityAdapter.TEMPERATURE
-    var favCitiesUtils = SearchUtils(DataUtils.mainUser.favCities)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(view.root)
-        updateCityAdapter(favCitiesUtils.getAllCities())
-        view.llFavCities.lvCities.setOnItemClickListener { _, _, position, _ ->
-            val intent = Intent(this, A5CityDetail::class.java).apply {
-                // We give the selected city
-                putExtra("cityPosition", position)
-                //And finally with another boolean we send if it becomes from the Fav List Activity or not
-                putExtra("favListActivity", true)
-            }
-            startActivity(intent)
-        }
-        view.llFavCities.svSearchCity.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                return false
-            }
+    override val cityUtils = SearchUtils(DataUtils.mainUser.favCities)
+    override val isFavouriteList = true
 
-            override fun onQueryTextChange(newText: String): Boolean {
-                updateCityAdapter(favCitiesUtils.searchCity(newText))
-                return false
-            }
-        })
-        view.llFavCities.rbgFilters.setOnCheckedChangeListener { _ , checkedId ->
-            when(checkedId) {
-                R.id.rbTemperature -> {
-                    displayType = CityAdapter.TEMPERATURE
-                }
-                R.id.rbRainProb -> {
-                    displayType = CityAdapter.RAIN_PROB
-                }
-                R.id.rbCondition -> {
-                    displayType = CityAdapter.CONDITION
-                }
-            }
-            updateCityAdapter(favCitiesUtils.getAllCities())
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        favCitiesUtils = SearchUtils(DataUtils.mainUser.favCities)
-        updateCityAdapter(favCitiesUtils.getAllCities())
-    }
-
-    private fun updateCityAdapter(cities: List<City>) {
-        val listView = findViewById<ListView>(R.id.lvCities)
-        val adapter = CityAdapter(this, cities, displayType)
-        listView.adapter = adapter
-    }
+    override fun provideLayout(): View = view.root
+    override fun provideListViewId(): Int = R.id.lvCities
+    override fun provideSearchViewId(): Int = R.id.svSearchCity
+    override fun provideRadioGroupId(): Int = R.id.rbgFilters
 }
--- app/src/main/java/com/example/weatherapp_mobapp/A4CitiesList.kt
+++ app/src/main/java/com/example/weatherapp_mobapp/A4CitiesList.kt
@@ -1,70 +1,16 @@
 package com.example.weatherapp_mobapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.SearchView
-import android.widget.Spinner
 import com.example.weatherapp_mobapp.databinding.ActivityA4CitiesListBinding
 
-class A4CitiesList : AppCompatActivity() {
+class A4CitiesList : BaseCityListActivity() {
 
     private val view by lazy { ActivityA4CitiesListBinding.inflate(layoutInflater) }
-    val citiesUtils = SearchUtils(DataUtils.mainUser.cities)
-    private var displayType = CityAdapter.TEMPERATURE
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(view.root)
-        updateCityAdapter(citiesUtils.getAllCities())
-        view.llCitiesList.lvCities.setOnItemClickListener { _, _, position, _ ->
-            val intent = Intent(this, A5CityDetail::class.java).apply {
-                // We give the selected city
-                putExtra("cityPosition", position)
-                //And finally with another boolean we send if it becomes from the Fav List Activity or not
-                putExtra("favListActivity", false)
-            }
-            startActivity(intent)
-        }
-        view.llCitiesList.svSearchCity.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                return false
-            }
+    override val cityUtils = SearchUtils(DataUtils.mainUser.cities)
+    override val isFavouriteList = false
 
-            override fun onQueryTextChange(newText: String): Boolean {
-                updateCityAdapter(citiesUtils.searchCity(newText))
-                return false
-            }
-        })
-        view.llCitiesList.rbgFilters.setOnCheckedChangeListener { _ , checkedId ->
-            when(checkedId) {
-                R.id.rbTemperature -> {
-                    displayType = CityAdapter.TEMPERATURE
-                }
-                R.id.rbRainProb -> {
-                    displayType = CityAdapter.RAIN_PROB
-                }
-                R.id.rbCondition -> {
-                    displayType = CityAdapter.CONDITION
-                }
-            }
-            updateCityAdapter(citiesUtils.getAllCities())
-        }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        updateCityAdapter(citiesUtils.getAllCities())
-    }
-
-    private fun updateCityAdapter(cities: List<City>) {
-        val listView = findViewById<ListView>(R.id.lvCities)
-        val adapter = CityAdapter(this, cities, displayType)
-        listView.adapter = adapter
-    }
-
+    override fun provideLayout(): View = view.root
+    override fun provideListViewId(): Int = R.id.lvCities
+    override fun provideSearchViewId(): Int = R.id.svSearchCity
+    override fun provideRadioGroupId(): Int = R.id.rbgFilters
 }
